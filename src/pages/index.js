632db08f622db66a57805debaebd0a63c4d0b29a
Chandima <chandima@Thikens-MacBook-Air.local>
2019-06-25 11:05:22 +0530

    title change

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Design+Code" />
+    <SEO title="cg+Code" />
     <h1>Design Code</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
